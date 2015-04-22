ea88b54e34ba57939530915dddb1f19634635a65
Schuyler Summers <schuyler.mail@gmail.com>
2015-04-22 16:13:07 -0600

    Test

--- TeamActivity0.js
+++ TeamActivity0.js
@@ -1,36 +1,38 @@
-			function toggleVisibility()
-			{
-				var e = document.getElementById("displayedText");
-				if (e.style.display == "block")
-				{
-					e.style.display = "none";	
-				}
-				else
-				{
-					e.style.display = "block";
-				}
-			}
 
-			function handleUpdateClick()
-			{
-				var text = document.getElementById("textInput").value;
-				var color = document.getElementById("colorInput").value;
-	
-				if (confirm("Are you sure you want to update?") == true)
-				{
-					appendText(text);
-					updateColor(color);
-				}
-			}
 
-			function appendText(text)
-			{	
-				var e = document.getElementById("displayedText");
-				e.innerHTML += text;
-			}
+function toggleVisibility()
+{
+	var e = document.getElementById("displayedText");
+	if (e.style.display == "block")
+	{
+		e.style.display = "none";	
+	}
+	else
+	{
+		e.style.display = "block";
+	}
+}
 
-			function updateColor(color)
-			{
-				var e = document.getElementById("displayedText");
-				e.style.backgroundColor = color;	
-			}
+function handleUpdateClick()
+{
+	var text = $("#textInput").val();
+	var color = $("#colorInput").val();
+
+	if (confirm("Are you sure you want to update?") == true)
+	{
+		appendText(text);
+		updateColor(color);
+	}
+}
+
+function appendText(text)
+{	
+	var e = $("#displayedText");
+	e.innerHTML();
+}
+
+function updateColor(color)
+{
+//	var e = $("#displayedText");
+//	e.style.backgroundColor = color;	
+}
